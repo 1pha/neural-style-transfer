f8434a40a584fc19a218d048eec06efe6129cbb0
Daehyun Cho <1phantasmas@korea.ac.kr>
2022-02-14 21:10:06 +0900

    <tfapp.view> pipeline proto

--- tfapp/views.py
+++ tfapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-# from .apps import TfappConfig
+from .apps import TfappConfig, image2tensor, tensor2image
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
@@ -8,39 +8,38 @@ import base64
 import io
 from io import BytesIO
 
+
 def main(request):
 
     return render(request, "index.html")
 
 @csrf_exempt
 def transfer(request):
-    # print(request.)
 
-    
     body_unicode = request.body.decode('utf-8')
     body = json.loads(body_unicode)
-    source_image = image_converter(body['source_image'])
+    content_image = image_converter(body['content_image'])
     style_image = image_converter(body['style_image'])
+    print(type(content_image))
 
-    print(source_image, style_image)
-    
+    content_tensor = image2tensor(content_image)
+    style_tensor = image2tensor(style_image)
+    # print(content_tensor, style_tensor)
+    gan_image = TfappConfig.model(content_tensor, style_tensor)[0]
 
+    gan_image = tensor2image(gan_image)
+    print(gan_image)
+    # print(tensor2image(gan_image))
 
-    #여기서 새로운 이미지 생성
-    #일단 임시로 source
-    gan_image = source_image
-
-
-    #다시 base64로
+    # 다시 base64로
     buffered = BytesIO()
+    gan_image.save("transfered.png", format="png")
     gan_image.save(buffered, format="png")
     img_str = base64.b64encode(buffered.getvalue()).decode()
 
     data = {
         "new_image": "data:image/jpeg;base64,"+img_str
     }
-
-
     return JsonResponse(data)
     
 
