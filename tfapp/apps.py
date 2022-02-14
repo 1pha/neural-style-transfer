a7905da8ddbcfc3f6ad96e87e0b7d8053421e289
Daehyun Cho <1phantasmas@korea.ac.kr>
2022-02-14 16:07:45 +0900

    <apps> add tensorflow hub

--- tfapp/apps.py
+++ tfapp/apps.py
@@ -1,6 +1,13 @@
 from django.apps import AppConfig
+import tensorflow_hub as hub
 
 
 class TfappConfig(AppConfig):
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'tfapp'
+    model = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/1')
+
+    def transfer(self, content, style):
+        stylized_image = self.model(content, style)
+        return stylized_image
