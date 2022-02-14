5491ec7b95c4434da8577743f58ac1628bc4220c
서중원 <js@seojung-won-ui-MacBookPro.local>
2022-02-14 17:14:20 +0900

    add communication feature between server and client

--- tfapp/apps.py
+++ tfapp/apps.py
@@ -1,12 +1,12 @@
 from django.apps import AppConfig
-import tensorflow_hub as hub
+# import tensorflow_hub as hub
 
 
 class TfappConfig(AppConfig):
 
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'tfapp'
-    model = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/1')
+    # model = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/1')
 
     def transfer(self, content, style):
         stylized_image = self.model(content, style)
--- tfapp/views.py
+++ tfapp/views.py
@@ -1,12 +1,53 @@
 from django.shortcuts import render
-from .apps import TfappConfig
-
+# from .apps import TfappConfig
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+from PIL import Image
+import base64
+import io
+from io import BytesIO
 
 def main(request):
 
     return render(request, "index.html")
 
-
+@csrf_exempt
 def transfer(request):
     # print(request.)
-    return render(request, "index.html")
+
+    
+    body_unicode = request.body.decode('utf-8')
+    body = json.loads(body_unicode)
+    source_image = image_converter(body['source_image'])
+    style_image = image_converter(body['style_image'])
+
+    print(source_image, style_image)
+    
+
+
+    #여기서 새로운 이미지 생성
+    #일단 임시로 source
+    gan_image = source_image
+
+
+    #다시 base64로
+    buffered = BytesIO()
+    gan_image.save(buffered, format="png")
+    img_str = base64.b64encode(buffered.getvalue()).decode()
+
+    data = {
+        "new_image": "data:image/jpeg;base64,"+img_str
+    }
+
+
+    return JsonResponse(data)
+    
+
+def image_converter(image):
+    image = image.split(",")[1]
+    image = base64.b64decode(image)
+    image = io.BytesIO(image)
+    image = Image.open(image)
+    return image
+
