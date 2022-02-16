3b82a2a96cf9e1a08ffe3352d22d3e391471acaf
Daehyun Cho <1phantasmas@korea.ac.kr>
2022-02-17 00:58:33 +0900

    <views> edit transfer

--- tfapp/views.py
+++ tfapp/views.py
@@ -13,6 +13,7 @@ def main(request):
 
     return render(request, "index.html")
 
+
 @csrf_exempt
 def transfer(request):
 
@@ -20,25 +21,19 @@ def transfer(request):
     body = json.loads(body_unicode)
     content_image = image_converter(body['content_image'])
     style_image = image_converter(body['style_image'])
-    print(type(content_image))
 
     content_tensor = image2tensor(content_image)
     style_tensor = image2tensor(style_image)
-    # print(content_tensor, style_tensor)
-    gan_image = TfappConfig.model(content_tensor, style_tensor)[0]
 
-    gan_image = tensor2image(gan_image)
-    print(gan_image)
-    # print(tensor2image(gan_image))
+    gan_tensor = TfappConfig.model(content_tensor, style_tensor)[0]
+    gan_image = tensor2image(gan_tensor)
 
-    # 다시 base64로
     buffered = BytesIO()
-    gan_image.save("transfered.png", format="png")
     gan_image.save(buffered, format="png")
     img_str = base64.b64encode(buffered.getvalue()).decode()
 
     data = {
-        "new_image": "data:image/jpeg;base64,"+img_str
+        "new_image": "data:image/jpeg;base64," + img_str
     }
     return JsonResponse(data)
     
